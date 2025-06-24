bcaf7f93f66fb0048f33e98e330f2f94c9e9b383
benicio <beniciobaldassare.dev@gmail.com>
2025-06-24 19:09:46 -0300

    Carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,9 +2,11 @@ import './CartWidget.css'
 
 export default function CartWidget(){
       return(
+        <>
           <div className="Carrito">
                <h4 className="logo-cart">Carrito</h4>
                <p className="cart-count">0</p>
           </div>
+        </>
       )
 }
