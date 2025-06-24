a870e0bf158d03020fe004997b9cb4f265c88797
benicio <beniciobaldassare.dev@gmail.com>
2025-06-24 19:08:34 -0300

    Factoreo de Productos

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,11 +2,13 @@ import Item from "../Item/Item.jsx"
 
 function ItemList ({ items = [] }) {
     return(
+    <>
         <div className="cards-conteiner" > 
         { items.map( prod => (
              <Item key={prod.id} prod={prod}/>
         ) ) }
         </div>
+    </>
     )
 }
 
